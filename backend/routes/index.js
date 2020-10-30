342354083707f57127298bd6265aac3da59a021b
hectorAlarcon <halarcfl39@alumnes.ub.edu>
2020-10-30 19:19:27 +0100

    DB connection Issue #22 #23 started

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,25 @@
 const express = require('express')
 const HelloWorld = require('../controllers/HelloWorld')
+const Users = require('../controllers/Users')
 
 const router = express.Router()
 
 
+/**
+ * LOGIN  
+ */
+//Customer
+
+//Restaurant
+router.post('/restaurant/login',Users.login)
+router.post('/restaurant/register',Users.register)
+
+
+
+/**
+ * These example will be deleted after Sprint 1. 
+ * HelloWorld test routes
+ */
 router.get('/helloworld', HelloWorld.getHelloWorld)
 router.post('/helloworld', HelloWorld.postHelloWorld)
 router.put('/helloworld', HelloWorld.putHelloWorld)
