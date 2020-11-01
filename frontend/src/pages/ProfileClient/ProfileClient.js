65d7e990065f447328c673c3d14872a0ddd157f7
Daniel <danielruizbasas@gmail.com>
2020-11-01 23:23:06 +0100

    US-15: Create profile page

--- frontend/src/pages/ProfileClient/ProfileClient.js
+++ frontend/src/pages/ProfileClient/ProfileClient.js
@@ -9,28 +9,31 @@ function ProfileClient() {
     <section className="login">
       <div className="loginContainer">
         
-        <label>Name</label>
-        <p>User's name</p>
 
-       
-
-        <label>E-mail</label>
-        <p>User's email</p>
+        <p style={{alignSelf: 'flex-end'}}><strong>Profile picture</strong></p>  
         
+        <label>Name</label>
+        <p>Pepito McDonald</p>
+        
+        <label>E-mail</label>
+        <p>pepitomcdonald@gmail.com</p>
 
         <label>DNI</label>
-        <p>User's DNI</p>
-        
-
+        <p>626646789R</p>
+      
         <label>Address</label>
-        <p>User's address</p>
+        <p>Carrer de Felip II 143, 1o 1a</p>
         
 
         <label>Tlf number</label>
-        <p>User's tlf number</p>
+        <p>681777564</p>
 
         <div className="btnContainer">
           <button> Edit profile</button>
+        </div>
+
+        <div className="btnContainer">
+          <button> Log out</button>
         </div>
          
       </div>
