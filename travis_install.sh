f9f09bfd18c3cc08215dbaa63e3db2f1e1faf1cc
Oriol Ornaque <32044766+oriolOrnaque@users.noreply.github.com>
2020-11-04 02:49:29 +0100

    Update travis_install.sh

--- travis_install.sh
+++ travis_install.sh
@@ -1,4 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 
-cd frontend && npm install && npm run build && cd ..
-cd backend && npm install && cd ..
+front_install() {
+	cd "frontend" || exit
+	npm install && npm run build
+	FRONT_INSTALL_RESULT=$?
+	cd ..
+}
+
+back_install() {
+	cd "backend" || exit
+	npm install
+	BACK_INSTALL_RESULT=$?
+	cd ..
+}
+
+front_tests
+if [ $FRONT_INSTALL_RESULT -e 0 ]; then
+    back_tests
+    if [ $BACK_INSTALL_RESULT -e 0 ]; then
+        exit 0
+    fi
+fi
+
+exit 127
