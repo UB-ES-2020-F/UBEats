12971c275655fc41c6e41ef5b9922db24e6985ee
Oriol Ornaque <32044766+oriolOrnaque@users.noreply.github.com>
2020-11-04 02:33:44 +0100

    Update travis_script.sh

--- travis_script.sh
+++ travis_script.sh
@@ -1,4 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
-#cd frontend && npm test; cd ..
-cd backend && npm test; cd ..
+front_tests() {
+	cd "frontend" || exit
+	npm test
+	FRONT_TESTS_RESULT=$?
+	cd ..
+}
+
+back_tests() {
+	cd "backend" || exit
+	npm test
+	BACK_TESTS_RESULT=$?
+	cd ..
+}
+
+#front_tests
+back_tests
+
+if [ $FRONT_TESTS_RESULT -ne 0 ] || [ $BACK_TESTS_RESULT -ne 0 ]; then
+	exit 127
+fi
+
+exit 0
