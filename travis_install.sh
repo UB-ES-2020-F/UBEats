fab915245970a4b64276d45363be5a9cac6ef196
Oriol <oornaqbl13@alumnes.ub.edu>
2020-11-04 12:03:50 +0100

    Travis scripts and SQL script improved with logs

--- travis_install.sh
+++ travis_install.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 
 front_install() {
-	cd "frontend" || exit
+	cd "frontend" || return 127
+	echo "[LOG] Installing the frontend"
 	npm install && npm run build
 	FRONT_INSTALL_RESULT=$?
+	echo "[LOG] Frontend installed with exit code: ${FRONT_INSTALL_RESULT}"
 	cd ..
+	return $FRONT_INSTALL_RESULT
 }
 
 back_install() {
-	cd "backend" || exit
+	cd "backend" || return 127
+	echo "[LOG] Installing backend"
 	npm install
 	BACK_INSTALL_RESULT=$?
+	echo "[LOG] Backend installed with exit code: ${BACK_INSTALL_RESULT}"
 	cd ..
+	return $BACK_INSTALL_RESULT
 }
 
-front_tests
-if [ $FRONT_INSTALL_RESULT -e 0 ]; then
-    back_tests
-    if [ $BACK_INSTALL_RESULT -e 0 ]; then
+front_install
+if [ $FRONT_INSTALL_RESULT -eq 0 ]; then
+    back_install
+    if [ $BACK_INSTALL_RESULT -eq 0 ]; then
         exit 0
     fi
 fi
--- travis_script.sh
+++ travis_script.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
 front_tests() {
-	cd "frontend" || exit
+	cd "frontend" || return 127
+	echo "[LOG] Executing frontend tests"
 	npm test
 	FRONT_TESTS_RESULT=$?
+	echo "[LOG] Frontend tests exit code: ${FRONT_TESTS_RESULT}"
 	cd ..
+	return $FRONT_TESTS_RESULT
 }
 
 back_tests() {
-	cd "backend" || exit
+	cd "backend" || return 127
+	echo "[LOG] Executing backend tests"
 	npm test
 	BACK_TESTS_RESULT=$?
+	echo "[LOG] Backend tests exit code: ${BACK_TESTS_RESULT}"
 	cd ..
+	return $BACK_TESTS_RESULT
 }
 
 #front_tests
