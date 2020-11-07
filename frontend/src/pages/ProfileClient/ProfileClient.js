86fdcbaa624ab2d78c99c8b4a8b3a072daa77df8
Daniel <danielruizbasas@gmail.com>
2020-11-07 16:08:55 +0100

    i15

--- frontend/src/pages/ProfileClient/ProfileClient.js
+++ frontend/src/pages/ProfileClient/ProfileClient.js
@@ -6,37 +6,8 @@ import { Link } from 'react-router-dom';
 
 function ProfileClient() {
   return (
-    <section className="login">
-      <div className="loginContainer">
-        
-
-        <p style={{alignSelf: 'flex-end'}}><strong>Profile picture</strong></p>  
-        
-        <label>Name</label>
-        <p>Pepito McDonald</p>
-        
-        <label>E-mail</label>
-        <p>pepitomcdonald@gmail.com</p>
-
-        <label>DNI</label>
-        <p>626646789R</p>
+    <section className="profile">
       
-        <label>Address</label>
-        <p>Carrer de Felip II 143, 1o 1a</p>
-        
-
-        <label>Tlf number</label>
-        <p>681777564</p>
-
-        <div className="btnContainer">
-          <button> Edit profile</button>
-        </div>
-
-        <div className="btnContainer">
-          <button> Log out</button>
-        </div>
-         
-      </div>
     </section>
 
   );
