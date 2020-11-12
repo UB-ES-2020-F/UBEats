5aff94b66a06447ba8a0f0038240fd3b3587b45e
Samuroldan <32091702+Samuroldan@users.noreply.github.com>
2020-11-12 16:33:47 +0100

    Build

---
+++ frontend/src/commons/components/RestPreviewGeneral.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Main.scss';
+import 'react-bootstrap';
+import'react-router-dom';
+import photo from '../../images/ubeats.png'
+
+function RestPreviewGeneral(props){
+
+    <div class="listings-grid-element">
+        <div class="image">
+            <img src={photo} alt="Restaurant image"></img>
+        </div>
+        <div class="text">
+            <div class="text-title">
+                <h3>[props.name]</h3>
+                <div class="info">
+                    <span> [props.time] MINS </span>
+                </div>
+            </div>
+        </div>
+        <div class="text-lower">
+            <span class="smallText">[props.desc]</span>
+        </div>
+    </div>
+
+}
+
+export default RestPreviewGeneral
---
+++ frontend/src/commons/components/RestPreviewMin.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Main.scss';
+import 'react-bootstrap';
+import'react-router-dom';
+import photo from '../../images/ubeats.png'
+
+function RestPreviewMin(props){
+
+    <div class="listings-grid-element">
+        <div class="image">
+            <img src={photo} alt="Restaurant image"></img>
+        </div>
+    </div>
+
+}
+
+export default RestPreviewMin
