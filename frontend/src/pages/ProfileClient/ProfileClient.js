81b98bc931de2a6158017cb8d6654e4bf614d4ac
Daniel <danielruizbasas@gmail.com>
2020-11-14 20:46:23 +0100

    issue15 commented

--- frontend/src/pages/ProfileClient/ProfileClient.js
+++ frontend/src/pages/ProfileClient/ProfileClient.js
@@ -37,18 +37,48 @@ function ProfileClient() {
   const [idioma, setIdioma] = useState(userInfo.idioma);
   const [email, setEmail] = useState(userInfo.email);
   const [showToast, setShowToast] = useState(false);
+  const [showToastFail, setShowToastFail] = useState(false);
+
+  {/**
+   * Checks if an input email is correctly formatted or not.
+   */}
+   
+  function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
+  
+  {/**
+   * Function triggered by the "Save Changes" button.
+   * Overwrites the user's info with the changed values.
+   * Also handles an incorrect email input.
+   */}
 
   function SaveChanges () {
     userInfo.idioma = idioma;
     userInfo.ubicacion = ubicacion;
-    userInfo.email = email;
-    setShowToast(true);
+    
+    if (validateEmail(email)) {
+      userInfo.email = email;
+      setShowToastFail(true);
+    }
+
+    else {
+      setShowToastFail(true);
+    }
+    
   }
+
+  
   
   return (
     <section className="profileClient">
 
       <Container className="profileContainer">
+
+        {/** First row:
+         * Profile picture and user's name and phone
+         */}
 
         <Row>
           <Col>
@@ -61,6 +91,10 @@ function ProfileClient() {
           </Col>
           
         </Row>
+
+        {/** Second row:
+         * Location selector, updates the component's state
+         */}
 
         <Row>
           <Col>
@@ -77,6 +111,10 @@ function ProfileClient() {
             />
           </Col>
         </Row>
+
+        {/** Third row:
+         * Language selector, updates the component's state
+         */}
 
        <Row>
           <Col>
@@ -95,6 +133,11 @@ function ProfileClient() {
           </Col>
         </Row>
 
+        {/** Fourth row:
+         * Display of the invitation code.
+         * It's not editable (and it shouldn't be)
+         */}
+
         <Row>
           <Col>
             <p>Invitation code</p>
@@ -104,6 +147,11 @@ function ProfileClient() {
             <p>{userInfo.codigoinvitacion}</p> 
           </Col>
         </Row>
+
+        {/** Fifth row:
+         * Input field for email changes.
+         * Default value: current email.
+         */}
 
         <Row>
           <Col>
@@ -122,6 +170,10 @@ function ProfileClient() {
           </Col>
         </Row>
 
+        {/** Save changes button
+         * Saves the current state into the user's data.
+         */}
+
         <Row>
           <Button 
             className="profileButton" 
@@ -132,11 +184,26 @@ function ProfileClient() {
           </Button>
         </Row>
 
+        {/** Toast triggered by the "Save changes" button.
+         * For user feedback purposes, correct input case.
+         */}
+
         <Row>
           <Toast onClose={() => setShowToast(false)} show={showToast} delay={3000} autohide>
             <Toast.Body>Your changes have been saved</Toast.Body>
           </Toast>
         </Row>
+
+        {/** Toast triggered by the "Save changes" button.
+         * For user feedback purposes, incorrect input case.
+         */}
+
+        <Row>
+          <Toast onClose={() => setShowToastFail(false)} show={showToastFail} delay={3000} autohide>
+            <Toast.Body>Enter a valid email, you goddamn cunt!</Toast.Body>
+          </Toast>
+        </Row>
+
 
         <Row>
           <p><strong>Authorised applications</strong></p>
