10575031710afcc7c221788fc4062834904b850f
Oriol <oornaqbl13@alumnes.ub.edu>
2020-11-19 17:47:33 +0100

    items tests refactor

--- backend/test/Item.js
+++ backend/test/Item.js
@@ -18,10 +18,100 @@ chai.use(chaiHttp);
 //Our parent block
 describe('Items', () => {
 
+  let user = {
+    email: 'jfbnmdlhfgb',
+    name: 'Oneqvqhbewfb',
+    CIF: '55455093R',
+    street: 'rgeqohugrehonij',
+    pass: 'qwerpui',
+    phone: '123456789',
+    type: 'restaurant',
+    url: 'iuwbeiqevqepiv',
+  }
+
+  var restaurant = {
+    email: 'cergouihn',
+    avaliability: 'amarillo',
+    visible: 'invisible',
+    iban: 'aaabbbcccdddeeefffggghhh',
+    allergens: 'rgheuiuognrce',
+  }
+
+  let category = {
+    category: 'rhijgrwejmhi',
+  }
+
+  var item = {
+    title: 'wreijgohn',
+    desc: 'rugrgfwn',
+    price: 3.1415,
+    visible: '1',
+    rest_id: 'cergouihn',
+    url: 'wrguiohenhigfec',
+    cat_id: 1,
+  }
+
+  var item_id = 1;
+
+  var saved_items_table;
+
+  before( async () => {
+    // save and clean the table
+    var query = format('DELETE FROM items RETURNING *')
+    var result = await pool.query(query)
+
+    saved_items_table = result.rows
+
+    //Insert a new user
+    var query = format('INSERT INTO users VALUES(%L) RETURNING *', Object.values(user))
+    var result = await pool.query(query)
+    restaurant.email = result.rows[0].email
+    //Insert a new restaurant
+    query = format('INSERT INTO restaurants VALUES(%L) RETURNING *', Object.values(restaurant))
+    result = await pool.query(query)
+    item.rest_id = result.rows[0].email
+    //Insert a new category
+    query = format('INSERT INTO categories VALUES(DEFAULT, %L) RETURNING *', Object.values(category))
+    result = await pool.query(query)
+    item.cat_id = result.rows[0].cat_id
+  })
+
+  after( async () => {
+    //Delete category
+    query = format('DELETE FROM categories WHERE cat_id = %s', item.cat_id)
+    result = await pool.query(query)
+    //Delete restaurant
+    query = format('DELETE FROM restaurants WHERE email = %L', item.rest_id)
+    result = await pool.query(query)
+    //Delete user
+    query = format('DELETE FROM users WHERE email = %L', item.rest_id)
+    result = await pool.query(query)
+
+    // restore the table
+    for(var i = 0; i < saved_items_table.length; ++i)
+    {
+      var query2 = format('INSERT INTO items VALUES(%L) RETURNING *', Object.values(saved_items_table[i]))
+      var result2 = await pool.query(query2)
+    }
+  })
+
+  beforeEach( async () => {
+    //Insert a new item
+    query = format('INSERT INTO items VALUES(DEFAULT, %L) RETURNING *', Object.values(item))
+    result = await pool.query(query)
+    item_id = result.rows[0].item_id
+  })
+
+  afterEach( async() => {
+    //Delete item
+    var query = format('DELETE FROM items WHERE item_id = %s', item_id)
+    var result = await pool.query(query)
+  })
+
   // TEST DE GET ENDPOINT
   describe('/GET ITEM', () => {
     // values by default, beforeEach will try to get values from the ddbb
-    var id = 1
+    /*var id = 1
     var rest = "rrr@gmail"
     var cat_id = 1
 
@@ -30,9 +120,9 @@ describe('Items', () => {
       "my_description",
       3.1415,
       '0',
-    ]
+    ]*/
 
-    beforeEach( async () => {
+    /*beforeEach( async () => {
       // get an existing restaurant from ddbb
       var result = await pool.query('SELECT * FROM restaurants')
       rest = result.rows[0].email
@@ -63,17 +153,18 @@ describe('Items', () => {
       db_values.pop()
       db_values.pop()
       db_values.pop()
-    })
+    })*/
 
     it('Get a known existing item. Should return 200', (done) => {
       chai.request(app)
-        .get('/api/items/'.concat(id))
+        .get('/api/items/'.concat(item_id))
         .set('content-type', 'application/x-www-form-urlencoded')
         .end((err, res) => {
             res.should.have.status(200);
             res.body.should.have.property('item')
             res.body.item.should.have.property('item_id')
             res.body.item.should.have.property('title')
+            res.body.item.title.should.equal(item.title)
             res.body.item.should.have.property('desc')
             res.body.item.should.have.property('price')
             res.body.item.should.have.property('visible')
@@ -89,7 +180,7 @@ describe('Items', () => {
       chai.request(app)
       //after the insert, id must be the largest item_id on the ddbb
       //and therefore, id+1 is non existent
-        .get('/api/items/'.concat(id+1))
+        .get('/api/items/'.concat(item_id+1))
         .set('content-type', 'application/x-www-form-urlencoded')
         .end((err, res) => {
             res.should.have.status(404);
@@ -102,39 +193,38 @@ describe('Items', () => {
 
   // TEST THE GET ALL ENDPOINT
   describe('/GET ITEMS', () => {
-    it('Get all existing items. Should return 200', (done) => {
+     it('Get all existing items. Should return 200', (done) => {
       chai.request(app)
         .get('/api/items')
         .set('content-type', 'application/x-www-form-urlencoded')
         .end((err, res) => {
             res.should.have.status(200);
             res.body.should.have.property('items');
-            res.body.items.should.be.an('array').to.have.lengthOf.above(0);
+            res.body.items.should.be.an('array').to.have.lengthOf(1);
             done();
           });
-
     });
   });
 
   // TEST THE GET ALL FOR A RESTAURANT
   describe('/GET RESTAURANT ITEMS', () => {
-    var rest = "rrr@gmail.com"
+    /*var rest = "rrr@gmail.com"
 
     beforeEach( async () => {
       // get an existing restaurant from ddbb
       var result = await pool.query('SELECT * FROM restaurants')
       rest = result.rows[0].email
-    })
+    })*/
 
 
     it('Get all items for a specific restaurant. Should return 200', (done) => {
       chai.request(app)
-        .get('/api/restaurant/'.concat(rest, '/items'))
+        .get('/api/restaurant/'.concat(item.rest_id, '/items'))
         .set('content-type', 'application/x-www-form-urlencoded')
         .end((err, res) => {
             res.should.have.status(200);
             res.body.should.have.property('items');
-            res.body.items.should.be.an('array').to.have.lengthOf.above(-1);
+            res.body.items.should.be.an('array').to.have.lengthOf(1);
             done();
           });
     });
@@ -164,19 +254,19 @@ describe('Items', () => {
     })
 
     it('Create a new item. All OK. Should return 200', (done) => {
-      let item = {
+      let item2 = {
         title: 'qwertyuiop',
         desc: 'qiur qejhfrg hqeoi qhfiqe he',
         price: 3.141592,
-        rest_id: 'rrr@gmail.com',
+        rest_id: item.rest_id,
         url: 'www.images.com/ofnforn.jpg',
-        cat_id: 4
+        cat_id: item.cat_id,
       }
 
       chai.request(app)
         .post('/api/items')
         .set('content-type', 'application/x-www-form-urlencoded')
-        .send(item)
+        .send(item2)
         .end((err, res) => {
           res.should.have.status(200);
           res.body.should.have.property('item')
@@ -192,18 +282,18 @@ describe('Items', () => {
     });
 
     it('Create a new item. Malformed body. Should return 403', (done) => {
-      let item = {
+      let item2 = {
         desc: 'qiur qejhfrg hqeoi qhfiqe he',
         price: 3.141592,
-        rest_id: 'rrr@gmail.com',
+        rest_id: item.rest_id,
         url: 'images.com/nfvosnf.jpg',
-        cat_id: 4
+        cat_id: item.cat_id,
       }
 
       chai.request(app)
         .post('/api/items')
         .set('content-type', 'application/x-www-form-urlencoded')
-        .send(item)
+        .send(item2)
         .end((err, res) => {
           res.should.have.status(403);
           res.body.should.have.property('message')
@@ -215,19 +305,19 @@ describe('Items', () => {
     });
 
     it('Create a new item. Invalid params. Should return 403', (done) => {
-      let item = {
+      let item2 = {
         title: 'qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq',
         desc: 'qiur qejhfrg hqeoi qhfiqe he',
         price: 3.141592,
-        rest_id: 'rrr@gmail.com',
+        rest_id: item.rest_id,
         url: 'www.images.com/ofnforn.jpg',
-        cat_id: 4
+        cat_id: item.cat_id,
       }
 
       chai.request(app)
         .post('/api/items')
         .set('content-type', 'application/x-www-form-urlencoded')
-        .send(item)
+        .send(item2)
         .end((err, res) => {
           res.should.have.status(403);
           res.body.should.have.property('message')
@@ -239,18 +329,18 @@ describe('Items', () => {
     });
 
     it('Create a new item. Invalid params. Should return 403', (done) => {
-      let item = {
+      let item2 = {
         title: 'eofuhvb',
         desc: 'qiur qejhfrg hqeoi qhfiqe he',
         price: -3.1415,
         url: 'images.com/nfvosnf.jpg',
-        cat_id: 4
+        cat_id: item.cat_id,
       }
 
       chai.request(app)
         .post('/api/items')
         .set('content-type', 'application/x-www-form-urlencoded')
-        .send(item)
+        .send(item2)
         .end((err, res) => {
           res.should.have.status(403);
           res.body.should.have.property('message')
