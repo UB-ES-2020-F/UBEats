1753776a1adf130fc383b71852e518f93bc0a690
AlbertCaTa <ac.tatjer@gmail.com>
2020-11-26 23:42:55 +0100

    prop passed to home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -48,7 +48,7 @@ const App = () => {
       <NavCustom  isLogged={isLogged} openSidebar={() => setSidebarOpen(!sidebarOpen)}/>
 
       <Switch>
-        <Route exact path="/" render={(props) => (<Home {...props} setRestaurantId={setRestSelected} setPicture={setRestPhoto}/>)} key='home'/>
+        <Route exact path="/" render={(props) => (<Home {...props} setRestaurantId={setRestSelected} setPicture={setRestPhoto} isLogged={isLogged}/>)} key='home'/>
         <Route path='/login' component={Login} key='login'/>
         <Route path='/registerclient' component={RegisterClient} key='register client'/>
         <Route path='/registerrestaurant' component={RegisterRestaurant} key='register restaurant'/>
