34b03d07f2ebd5fa2659a9287afbafb4a2bb2d1b
AlbertCaTa <ac.tatjer@gmail.com>
2020-11-28 14:14:07 +0100

    ProfileClient only visible if logged. User info fed to profileClient components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -43,7 +43,6 @@ const App = () => {
   
   return (
     <Router history={history}>
-      {console.log(currentUser)}
       {sidebarOpen ? (<GeneralSidebar isOpen={sidebarOpen} onOpen={setSidebarOpen} isLogged={isLogged} user={currentUser} key='sidebar'/>):(<div/>)}
       <NavCustom  isLogged={isLogged} openSidebar={() => setSidebarOpen(!sidebarOpen)}/>
 
@@ -53,7 +52,7 @@ const App = () => {
         <Route path='/registerclient' component={RegisterClient} key='register client'/>
         <Route path='/registerrestaurant' component={RegisterRestaurant} key='register restaurant'/>
         <Route path='/registerdeliveryman' component={RegisterDeliveryman} key='register deliveryman'/>
-        <Route path='/profileclient' component={ProfileClient} key='profile client'/>
+        {isLogged && <Route path='/profileclient' render={(props) => (<ProfileClient {...props} user={currentUser}/>)} key='profile client'/>}
         <Route path='/profilerestaurant' render={(props) => (<ProfileRestaurant {...props} rest_id={restSelected} restaurantPhoto={restPhoto}/>)} key='profile restaurant'/>
       </Switch>
       
