c8d3a1886fee55c06b58ba57af1a85e7e914bd16
Oriol <oornaqbl13@alumnes.ub.edu>
2020-11-29 16:58:25 +0100

    added basic profiling

--- travis_script.sh
+++ travis_script.sh
@@ -20,11 +20,43 @@ back_tests() {
 	return $BACK_TESTS_RESULT
 }
 
+profiling() {
+	cd "backend" || return 127
+
+	#Start app in background
+	node --prof app.js &
+	#Save the process id on the background
+	APP_PID=$!
+
+	echo "[LOG] Profiling"
+
+	curl -X GET "http://localhost:3000/api/qwertyuiop/users"
+	curl -X GET "http://localhost:3000/api/items"
+	echo $(ab -k -c 20 -n 20 "http://localhost:3000/api/qwertyuiop/users" | grep -A11 'Concurrency Level')
+	echo $(ab -k -c 20 -n 20 "http://localhost:3000/api/items" | grep -A11 'Concurrency Level')
+
+	#Kill the node process once all checks have been done
+	kill ${APP_PID}
+
+	#Read the profile results and export them to text format
+	node --prof-proces isolate-0x*-v8.log > profiling.log
+
+	#Grep the summary
+	echo $(grep -A5 '[Summary]' profiling.log)
+
+	#Clean up
+	rm isolate-0x*-v8.log profiling.log
+
+	cd ..
+}
+
 # [IMPORTANT] Don't uncomment front_tests if there are no tests for the frontend
 # 			  because npm test will fail and stop the continuous integration process
 FRONT_TESTS_RESULT=0
 #front_tests
 back_tests
+# QA tests and performance checks
+profiling
 
 if [ $FRONT_TESTS_RESULT -eq 0 ] && [ $BACK_TESTS_RESULT -eq 0 ]; then
 	exit 0
