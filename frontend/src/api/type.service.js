bf71b7a9fe19765c52ef95d12b2c4b215951ac68
hectorAlarcon <halarcfl39@alumnes.ub.edu>
2020-12-01 11:42:59 +0100

    fixed all API_URL

--- frontend/src/api/type.service.js
+++ frontend/src/api/type.service.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_URL_DEV = "http://localhost:3000/api/types";
 const API_URL_BUILD = "https://cors-anywhere.herokuapp.com/http://ub-gei-es-ubeats-clone.herokuapp.com/api/restaurants";
-const API_URL = API_URL_DEV;
+const API_URL = API_URL_BUILD;
 
 
 //Get all restaurant types {type_id, type_name}
