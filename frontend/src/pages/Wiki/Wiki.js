45e027283471332aecdf964941bd4ebc6a915a03
AlbertCaTa <ac.tatjer@gmail.com>
2020-12-15 14:46:23 +0100

    spell checked

--- frontend/src/pages/Wiki/Wiki.js
+++ frontend/src/pages/Wiki/Wiki.js
@@ -24,7 +24,7 @@ function Wiki() {
                         </Nav.Item>
                         
                         <Nav.Item>
-                            <Nav.Link eventKey="customerFeaturesTab"> Features</Nav.Link>
+                            <Nav.Link eventKey="customerFeaturesTab">Features</Nav.Link>
                         </Nav.Item>
 
                         
@@ -53,8 +53,8 @@ function Wiki() {
                                     <Accordion.Collapse eventKey="0">
                                     <Card.Body>
                                         
-                                            Have you ever been sitting on your sofa and thinking 'Oh man I'd really like some pizza' but didn't want to go to a restaurant or prepared all by yourself? 
-                                            That's why we have developed UbEats, a website where you can order from your favourities restaurants in the comfort of your home!
+                                            Have you ever been sitting on your sofa and thought 'Oh man I'd really like some pizza' but didn't want to go to a restaurant or prepare it all by yourself? 
+                                            That's why we have developed UbEats, a website where you can order from your favourite restaurants in the comfort of your home!
                                             It is as simple as going to our website https://ub-gei-es-ubeats-clone.herokuapp.com/, log in if you already have an account and start ordering with only one click!
                                     </Card.Body>
                                     </Accordion.Collapse>
@@ -68,8 +68,8 @@ function Wiki() {
                                     </Accordion.Toggle>
                                     <Accordion.Collapse eventKey="1">
                                     <Card.Body>
-                                            UbEats is a webstie clone of Ubereats, the purpose of the creation of such website is purely educational on how to recreate a website from scratch. Therefore the license applied to UbEats project is MIT and 
-                                            all credits to  @2020 Uber Technologies Inc. on https://ubereats.com. The main differences find between these two are due to lack of time or money ( pe : hosting, domain) but the whole UbEats tries to be as similar to Ubereats 
+                                            UbEats is a website clone of Ubereats, the purpose of the creation of this website is purely educational, to recreate a website from scratch. Therefore the license applied to UbEats project is MIT and 
+                                            all credits to  @2020 Uber Technologies Inc. on https://ubereats.com. The main differences between these two are due to lack of time and money ( pe : hosting, domain) but UbEats strives to be as similar to Ubereats 
                                             as possible. 
                                     </Card.Body>
                                     </Accordion.Collapse>
@@ -101,23 +101,23 @@ function Wiki() {
                                     <Accordion.Collapse eventKey="customerFeaturesTab-1">
                                     <Card.Body>
                                         <p>
-                                            As a customer, a registered user, we have several features to be talked about and are worth mentioning.
-                                            First of all, as a visitor you can choose to surf around the website with almost no restriction at all. But at any moment you can sign in (if you already have an accout) or register.
+                                            As a customer, a registered user, there are several features worth mentioning.
+                                            First of all, as a visitor, not logged, you can choose to surf around the website with almost no restriction at all. But at any moment you can sign in (if you already have an accout) or register.
                                             In terms of registration there are 3 possibilities, one for customers (main purpose), one for restaurants and one for users who wants to work as deliveryman. Even though there are 3 registration pages, 
                                             the login is shared through all of them but the website has only the customer view (first one).
                                         </p>
 
                                         <p>
-                                            On entering onto the website for the first time we can see a similar style to Ubereats, in fact most of the style is based on ubereats official website. The page shown as entering is called Homepage, 
-                                            here we can explore the affilliated restaurants easily, seeing for each restaurant  its name ( per example 'McDonald') and a representative image of it. What's more, each restaurant is categorized into one of the 13 possible categories,
+                                            On entering to the website for the first time we can see a similar style to Ubereats, in fact most of the style is based on ubereats official website. The page shown upon entering is called Homepage, 
+                                            here we can explore the affilliated restaurants easily, showing each restaurant name (for example 'McDonald') and a representative image of it. What's more, each restaurant is categorized into one of the 13 possible categories,
                                             allowing viewers to reduce the search time and aim towards its wishes. In fact, each icon filters restaurants and shows only the restaurants belonging to that specific category. It is worth mentioning that the homepage shows 
                                             a variety of top restaurants and every restaurant at the same time the user continues to scroll down.
                                         </p>
                                         <p>
                                             A user can click on whichever restaurant he/she wants ( either the image or the title) and will be redirected to the restaurant's homepage. 
-                                            The restaurant homepage is the page where all the restaurants informations lays on, as a matter of fact the page can be break down into 3 sections. The top section shows useful information of the restaurant, such as 
-                                            the street, the price range, the time to delivery or the type of restaurant it belongs to ('American','Sushi', etc). Just below there is an horizontal display composed of the categories of the products it contains.
-                                            These category titles bring the user to the corresponding category on which each product will be accordingly displayed.  And here we find the last section, which is all the restaurant products shown with a title, a description
+                                            The restaurant homepage is the page where all the restaurants information lays on, as a matter of fact the page can be broken down into 3 sections. The top section shows useful information of the restaurant, such as 
+                                            the street, the price range, the delivery time or the type of restaurant it belongs to ('American','Sushi', etc). Just below there is an horizontal display composed of the categories of the products it contains.
+                                            These category titles bring the user to the corresponding category on which each product will be accordingly displayed. And here we find the last section, which is all the restaurant products shown with a title, a description
                                             and an image, everything selected by the restaurant, both the products and the categories. 
                                         </p>
                                     </Card.Body>
@@ -132,12 +132,10 @@ function Wiki() {
                                     </Accordion.Toggle>
                                     <Accordion.Collapse eventKey="customerFeaturesTab-2">
                                     <Card.Body>
-                                        
                                         <p>
                                             Moreover, if the user is registered and logged in, he/she may mark any restaurant as 'Favourite', then creating a dynamic list on which every favourite restaurant will be shown. There is also a page where all 
-                                            the favourites are displayed. The idea behind this is to allow registered customers to have an benefit as with regard to non-registered ones, helping them to browse and find appealing meals faster.
+                                            the favourites are displayed. The idea behind this is to allow registered customers to have and benefit as with regard to non-registered ones, helping them to browse and find appealing meals faster.
                                         </p>
-
                                         <p>
                                             In order to improve the user experience there is a working navigation bar, containg a reference to homepage UbEats, a search bar on which users can type and browse for any restaurant our servers have. Also it has
                                             a menu button which on click it will display a sidebar that will show either some Log In / Registering options in case the user is not logged in or its personal information with some extra functionalities described before.
@@ -185,7 +183,7 @@ function Wiki() {
                                     <Accordion.Collapse eventKey="technologiesUsed-1">
                                     <Card.Body>
                                         <p>
-                                            First of all, the project itself uses <a href='https://nodejs.org/en/' target='_blank'>Node.js </a>  as its core. 
+                                            First of all, the project itself uses <a href='https://nodejs.org/en/' target='_blank'>Node.js</a>  as its core. 
                                         </p>
                                         <p>
                                             To develop the visual website we used on top of node <a href='https://reactjs.org/' target='_blank'>React.js</a>  and <a href='https://react-bootstrap.github.io/' target='_blank'>Bootstrap libraries</a>  to agilize the design process.
@@ -197,7 +195,7 @@ function Wiki() {
                                             To create a profitable continous integration and continous developtment pipelen we have chosen <a href='https://travis-ci.com/' target='_blank'>Travis.com </a> as it is free for Students and has incredibily useful functionalities.
                                         </p>
                                         <p>
-                                            To test and assure our code is good enough we added a dynamic quality assurrance (Profiling) and an static one (ESLinting). Also each test run, either in backend or frontend is checked and if anything fails 
+                                            To test and assure our code is good enough we added a dynamic quality assurrance (Profiling) and an static one (ESLinting). Also each test that runs either on backend or on frontend is checked and if anything fails 
                                             travis catch it and prevents that code to be deployed.
                                         </p>
                                         <p>
@@ -220,14 +218,14 @@ function Wiki() {
                                             Second part of the techonlogies used is attached to the working part, in other words, how we create a work flow.
                                         </p>
                                         <p>
-                                            To have a big picture of what have been done on each Sprint and what we are going to do we have Trello. Here we have created Epics, on which we derivated the most ambitious UserStories with its 
+                                            To have a big picture of what have done on each Sprint and what we are going to do we have Trello. Here we have created Epics, on which we derivated the most ambitious UserStories with its 
                                             Acceptance Criteria. We have tried to be as exhaustive as possible, adding images if necesarry to specify how things have to be.
-                                            We used GitHub as our 'coding host' and as our Project Kanban on which our issues/tasks will be added to the project, assigned and discussed if necesarry. Everything that is needed to do a task is inside the 
+                                            We used GitHub as our 'coding host' and as our Project Kanban on which our issues/tasks will be added to the project, assigned and discussed if necesarry. Everything that is needed to do in a task is inside the 
                                             issue.
                                         </p>
                                         <p>
                                             Here we would like to explain the evolution of our card issues, how they started and how they have been evolving through the entire process.
-                                            Initially cars had a title composed of [UserStoy-#id] Title of the task, a simple description, the sprint number (milestone), the people that has been assigned to and some descriptive labels.
+                                            Initially cars had a title composed of [UserStoy-#id] Title of the task, a simple description, the sprint number (milestone), the people that have been assigned to and some descriptive labels.
                                             The problem with this is that the description was not exhaustive, creating then a miscommunication on what is required to mark the issue as Done. Here we found another problem, what is DONE was different
                                             for each member and we had to define as a team what a Done Issue is and what is not. Moreover everytime we finished an Sprint we spent too much time counting and keeping track of the real hours and the 
                                             planned hours. For that reason we added a new element in the title : #HOURS that are the planned hours of the task. Also the description began to be as detailed as needed, which help a lot when members 
@@ -284,10 +282,10 @@ function Wiki() {
                                             0. All Trello US with is corresponding Acceptance Criteria are updated as well as the priority of each one.
                                         </p>
                                         <p>
-                                            1. Create new tasks from most important US during Sprint beginning
+                                            1. Create new tasks from most important US during Sprint beginning.
                                         </p>
                                         <p>
-                                            2. Once all the new task have been created each member choose the tasks that will require at least the minimum amount of hours established (8 hours). Each task has 
+                                            2. Once all the new tasks have been created each member chooses the tasks that will require at least the minimum amount of hours established (8 hours).
                                         </p>
                                         <p>
                                             3. Once tasks have been defined and assigned, members create its corresponding GitHub issue on which the product owner will add the Acceptance Criteria.
@@ -328,22 +326,22 @@ function Wiki() {
                                         <div className='ml-3'>
                                             <ul >
                                                 <li>
-                                                    <strong> <a href='https://github.com/danielruizbasas' target="_blank"> Daniel Ruiz </a> </strong>: Frontend developer.
+                                                    <strong> <a href='https://github.com/danielruizbasas' target="_blank"> Daniel Ruiz</a> </strong>: Frontend developer.
                                                 </li>
                                                 <li>
-                                                    <strong> <a href='https://github.com/Samuroldan' target="_blank">Samuel Roldan</a> </strong>: Frontend developer (mostly visual aspects).
+                                                    <strong> <a href='https://github.com/Samuroldan' target="_blank">Samuel Roldan</a> </strong>: Frontend developer (visual aspects).
                                                 </li>
                                                 <li>
-                                                    <strong> <a href='https://github.com/Rbb93' target="_blank">Ruben Blanco</a></strong>: Backend developer ( database schema designer).
+                                                    <strong> <a href='https://github.com/Rbb93' target="_blank">Ruben Blanco</a></strong>: Backend developer (database schema designer).
                                                 </li>
                                                 <li>
-                                                    <strong> <a href='https://github.com/aldakata' target="_blank">Albert Catala</a></strong>: Frontend developer of the team (functional and visual aspects).
+                                                    <strong> <a href='https://github.com/aldakata' target="_blank">Albert Catalan</a></strong>: Frontend developer (functional and visual aspects).
                                                 </li>
                                                 <li>
-                                                    <strong><a href='https://github.com/oriolOrnaque' target="_blank"> Oriol Ornaque</a></strong>: DevOps of the team, Backend developer.
+                                                    <strong><a href='https://github.com/oriolOrnaque' target="_blank"> Oriol Ornaque</a></strong>: DevOps of the team, Backend developer, QA.
                                                 </li>
                                                 <li>
-                                                    <strong> <a href='https://github.com/hectorAlarcon' target="_blank">Hector Alarcon</a></strong>: Product owner of the team, Backend developer.
+                                                    <strong> <a href='https://github.com/hectorAlarcon' target="_blank">Hector Alarcon</a></strong>: Product owner of the team, Backend developer, QA.
                                                 </li>
                                             </ul>
                                         </div>
